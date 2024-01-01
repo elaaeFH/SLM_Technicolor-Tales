d9d40a9bb5a9937fc19f2b146d2e3a6ccf14e740
elaaeFH <ic23b021@technikum-wien.at>
2024-01-01 21:46:43 +0100

    Player class with the needed variables added

---
+++ TicTacToe/src/main/java/TechnicolorTales/Player.java
@@ -0,0 +1,13 @@
+package TechnicolorTales;
+
+public class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
